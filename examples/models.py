65ec9678619a7cd2e36486d55d8a63db82ab5ae6
peterw2333 <2449810783@qq.com>
2023-03-07 01:53:24 +0000

    add mnist toy example

---
+++ examples/models.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_size=784, hidden_size=100, num_classes=10):
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.l1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_classes)
+        self.softmax = nn.Softmax()
+    
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.relu(x)
+        x = self.l2(x)
+        x = self.softmax(x)
+        return x
+
+class LeNet5(torch.nn.Module):
+     
+    def __init__(self):   
+        super(LeNet5, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2, bias=True)
+        self.max_pool_1 = torch.nn.MaxPool2d(kernel_size=2)
+        self.conv2 = torch.nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0, bias=True)
+        self.max_pool_2 = torch.nn.MaxPool2d(kernel_size=2)
+        self.fc1 = torch.nn.Linear(16*5*5, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
+        
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.conv1(x))  
+        x = self.max_pool_1(x)
+        x = torch.nn.functional.relu(self.conv2(x))
+        x = self.max_pool_2(x)
+        x = x.view(-1, 16*5*5)
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
